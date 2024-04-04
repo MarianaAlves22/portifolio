2a8ebc029095840b1891f656c293036fda09332f
Mariana Alves <Mary_A2903@hotmail.com>
2024-04-04 16:20:39 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/MarianaAlves22.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/MarianaAlves22.png" />
-    <Titulo fontSize={20}>Mariana Alves</Titulo>
+    <SidebarContainer>
+      <Avatar></Avatar>
+      <Titulo fontSize={20}>Mariana Alves</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        MarianaAlves22
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Estudante front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
